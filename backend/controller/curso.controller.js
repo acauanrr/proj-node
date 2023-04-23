819bb4ce4e96bea3f74bbe726cde6f2973f545fe
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-04-23 08:18:22 -0400

    Create get, update and delete methods for curso controller

--- backend/controller/curso.controller.js
+++ backend/controller/curso.controller.js
@@ -8,10 +8,45 @@ export class CursoController {
 		return cursos;
 	}
 
+	async get(cursoDTO) {
+		const cursos = await this.curso.findAll({
+			where: {
+				id: cursoDTO.id,
+			},
+		});
+		return cursos;
+	}
+
 	async adicionar(cursoDTO) {
 		try {
 			console.log(cursoDTO);
 			await this.curso.create(cursoDTO);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async update(cursoDTO, newCursoDTO) {
+		try {
+			console.log(cursoDTO);
+			await this.curso.update(newCursoDTO, {
+				where: {
+					id: cursoDTO.id,
+				},
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async delete(cursoDTO) {
+		try {
+			console.log(cursoDTO);
+			await this.curso.destroy({
+				where: {
+					id: cursoDTO.id,
+				},
+			});
 		} catch (error) {
 			console.log(error);
 		}
